6ea1d020cbadc7927d0a2b5c8f732e99e04a348f
violettomania <danielpolya@protonmail.com>
2024-01-17 17:34:37 +0100

    feature: add Hero component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,9 @@
+import Hero from './Hero';
+
 export default function App() {
   return (
     <main>
-      <section className='hero'>
-        <div className='hero-center'>
-          <div className='hero-title'>
-            <h1>contentful CMS</h1>
-            <p>
-              Pitchfork schlitz tonx, coloring book celiac tousled succulents
-              ascot affogato cardigan jianbing crucifix seitan. Synth man braid
-              everyday carry try-hard pour-over keffiyeh slow-carb sriracha
-              chillwave banjo gochujang kinfolk small batch mustache.
-            </p>
-          </div>
-          <div className='img-container'>
-            <img
-              src='/assets/hero-d50f9f18.svg'
-              alt='woman and the browser'
-              className='img'
-            />
-          </div>
-        </div>
-      </section>
+      <Hero />
       <section className='projects'>
         <div className='title'>
           <h2>Projects</h2>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,1 +1,17 @@
-export default function Hero() {}
+export default function Hero() {
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <div className='hero-title'>
+          <h1>contentful CMS</h1>
+          <p>
+            Pitchfork schlitz tonx, coloring book celiac tousled succulents
+            ascot affogato cardigan jianbing crucifix seitan. Synth man braid
+            everyday carry try-hard pour-over keffiyeh slow-carb sriracha
+            chillwave banjo gochujang kinfolk small batch mustache.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
